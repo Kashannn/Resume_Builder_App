15a645ba86a3f8649a527fcdf867bb2cd6a2695b
saqibjaved <saqibjaved19078@gmail.com>
2024-08-21 20:28:37 +0500

    cv

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,15 +1,18 @@
-import 'package:cvapp/utils/constant/app_images_constant.dart';
-import 'package:cvapp/view/customize_template_screen.dart';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 import '../utils/components/custom_bottom_navigation_bar.dart';
 import '../utils/constant/app_colors.dart';
+import '../utils/constant/app_images_constant.dart';
 import '../utils/constant/app_textstyle_constant.dart';
-import 'cv_template_screen.dart';
+import 'customize_template_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -19,6 +22,29 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String? localPdfPath;
+
+  @override
+  void initState() {
+    super.initState();
+    loadPdfFromAssets();
+  }
+
+  Future<void> loadPdfFromAssets() async {
+    try {
+      final ByteData data = await rootBundle.load('assets/templates/template1.pdf');
+      final Uint8List bytes = data.buffer.asUint8List();
+      final Directory tempDir = await getTemporaryDirectory();
+      final File file = File('${tempDir.path}/template1.pdf');
+      await file.writeAsBytes(bytes, flush: true);
+      setState(() {
+        localPdfPath = file.path;
+      });
+    } catch (e) {
+      print('Error loading PDF: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
@@ -32,12 +58,10 @@ class _HomeScreenState extends State<HomeScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                'CV marker',
+                'CV Maker',
                 style: isDarkMode ? mStyleWhite20600 : mStyleBlack20600,
               ),
-              SizedBox(
-                height: 20.h,
-              ),
+              SizedBox(height: 20.h),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -53,21 +77,27 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ],
               ),
-              SizedBox(
-                height: 20.h,
-              ),
-              GestureDetector(
-                onTap: () => Get.to(CustomizeTemplateScreen()),
-                child: Container(
-                  height: 200.h,
-                  width: 200.w,
-                  decoration: BoxDecoration(
+              SizedBox(height: 20.h),
+              localPdfPath != null
+                  ? GestureDetector(
+                onTap: (){Get.to(CustomizeTemplateScreen(
+                  filePath: localPdfPath!,
+                ));},
+                    child: Container(
+                                    padding: EdgeInsets.all(10.w),
+                                    height: 250.h,
+                                    width: 193.w,
+                                    decoration: BoxDecoration(
                     color: Color(0xFF151A25),
                     borderRadius: BorderRadius.circular(10.r),
-                  ),
-                  child: CVTemplateScreen(),
-                ),
-              )
+                                    ),
+                                    child: PDFView(
+                    filePath: localPdfPath!,
+
+                                    ),
+                                  ),
+                  )
+                  : Center(child: CircularProgressIndicator()),
             ],
           ),
         ),
